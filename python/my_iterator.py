cfc36de25af3c4ee01d744e98783a583c93949aa
infuq <infuq217@outlook.com>
2021-03-07 16:37:03 +0800

    自定义迭代器

---
+++ python/my_iterator.py
@@ -0,0 +1,33 @@
+# /usr/env/python
+
+from collections.abc import Iterator
+
+class Student():
+
+    def __init__(self, score):
+        self.score = score
+
+    def __iter__(self):
+        return MyIterator(self.score)
+
+
+class MyIterator(Iterator):
+
+    def __init__(self, score):
+        self.score = score
+        self.index = 0
+
+    def __next__(self):
+        try:
+            result = self.score[self.index]
+        except IndexError:
+            raise StopIteration
+        self.index = self.index + 1
+        return result        
+
+if __name__ == '__main__':
+    
+    stu = Student([89, 75, 92, 69])
+    for item in stu:
+        print(item)
+
