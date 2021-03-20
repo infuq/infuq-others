7579573894695488f4f4febf5ee06936727dd7d1
infuq <infuq217@outlook.com>
2021-03-21 03:32:17 +0800

    Python正则 sub split findall search match

---
+++ Python/process_t/t_3.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+'''
+结束子进程
+'''
+
+import multiprocessing
+import time
+
+class MyProcess(multiprocessing.Process):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    def run(self):
+        print("PID={}, Name={}".format(multiprocessing.current_process().pid, multiprocessing.current_process().name))
+        time.sleep(5)
+        print('子进程结束...')
+
+
+
+if __name__ == '__main__':
+
+    process = MyProcess('My_0')
+    process.start()
+
+    # 主进程等待3秒钟, 如果子进程还存活, 那么结束子进程
+    time.sleep(3)
+    if process.is_alive():
+        process.terminate()
+
