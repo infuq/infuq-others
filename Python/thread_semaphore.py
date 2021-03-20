bfdc984c588a07381a41f67557ceccfa9252936b
infuq <infuq217@outlook.com>
2021-03-20 23:51:31 +0800

    Python semaphore学习使用

---
+++ Python/thread_semaphore.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+'''
+Semaphore的使用
+'''
+
+from threading import Semaphore
+import threading
+import time
+
+
+def print_info(sem, index):
+    sem.acquire()
+    time.sleep(2)
+    print(threading.currentThread().getName(), '->', index)
+    sem.release()
+
+
+if __name__ == '__main__':
+    sem = Semaphore(3)
+    for i in range(20):
+        t = threading.Thread(target=print_info, args=(sem, i))
+        t.start()
+
