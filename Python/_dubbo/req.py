7e210722e747d72ec166638107fbdf894deea985
infuq <infuq217@outlook.com>
2021-04-05 01:20:34 +0800

    ...

--- Python/_dubbo/req.py
+++ Python/_dubbo/req.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 
 class Req(object):
-    # http://dubbo.apache.org/zh-cn/blog/dubbo-protocol.html
-    magic = bytearray(2)
-    flag = bytearray(1)
-    status = bytearray(1)
-    req_id = bytearray(8)
-    body_len = bytearray(4)
-    body = None  # body'length
-    data = bytearray(16)
-
+    
     def __init__(self):
-        pass
+        # http://dubbo.apache.org/zh-cn/blog/dubbo-protocol.html
+        self.magic = bytearray(2)
+        self.flag = bytearray(1)
+        self.status = bytearray(1)
+        self.req_id = bytearray(8)
+        self.body_len = bytearray(4)
+        self.body = None  # body'length
+        self.data = bytearray(16)
 
 
 if __name__ == '__main__':
