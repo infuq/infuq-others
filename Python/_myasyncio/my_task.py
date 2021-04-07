1029d3afe1b7c8a449d30a4e458179a55bab83f3
infuq <infuq217@outlook.com>
2021-04-07 21:05:01 +0800

    ...

--- Python/_myasyncio/my_task.py
+++ Python/_myasyncio/my_task.py
@@ -6,24 +6,18 @@ import time
 
 
 async def mock():
-    print("---1---", threading.currentThread().getName())
     await asyncio.sleep(3)
-    print("---2---", threading.currentThread().getName())
     return "模拟"
 
 
 async def main():
-    print("---3---", threading.currentThread().getName())
     task = asyncio.create_task(mock())
-    print("---4---", threading.currentThread().getName())
     ret = await task
 
 
 if __name__ == '__main__':
-    print("---5---", threading.currentThread().getName())
     asyncio.run(main())
-    print("---6---", threading.currentThread().getName())
-    time.sleep(6)
-    print("---7---", threading.currentThread().getName())
+
+    asyncio.new_event_loop()
 
 
