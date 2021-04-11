7cd6d6b010854baf9a1e568b43940fd8517fc63d
infuq <infuq217@outlook.com>
2021-04-11 22:28:18 +0800

    ...

--- Python/myutil/my_download.py
+++ Python/myutil/my_download.py
@@ -12,5 +12,5 @@ def download(url):
     
 
 if __name__ == '__main__':
-    url = "https://www.bilibili.com/video/BV1HJ411z7ZU?spm_id_from=333.824.b_76696577626f785f7265706f7274.1"
+    url = "https://www.bilibili.com/video/BV1Cy4y1C7WY?spm_id_from=333.824.b_76696577626f785f7265706f7274.1"
     download(url)
