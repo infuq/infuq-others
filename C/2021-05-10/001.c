02b5d6fbb1add890160d1ce1a2f082d21d914446
infuq <infuq217@outlook.com>
2021-05-12 00:51:37 +0800

    ...

--- C/2021-05-10/001.c
+++ C/2021-05-10/001.c
@@ -17,7 +17,6 @@ void build(int index, int left, int right)
 	}
 
 	int mid = left + (right - left) / 2;
-	printf("%d\n", mid);
 	build(2 * index + 1, left, mid);
 	build(2 * index + 2, mid + 1, right);
 
@@ -47,6 +46,30 @@ void update(int index, int left, int right, int idx, int v)
 
 }
 
+// 查询
+int query(int index, int left, int right, int lq, int rq)
+{
+
+	// 第一种情况
+	if (left == lq && right == rq)
+		return tree[index];
+
+
+	int mid = left + (right - left) / 2;
+
+	// 第二种情况
+	if (rq <= mid)
+		return query(2 * index + 1, left, mid, lq, rq);
+	// 第三种情况
+	else if (mid + 1 <= lq)
+		return query(2 * index + 2, mid + 1, right, lq, rq);
+
+	// 第四种情况
+	return query(2 * index + 1, left, mid, lq, mid) + query(2 * index + 2, mid + 1, right, mid + 1, rq);
+
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -62,14 +85,19 @@ int main(int argc, char *argv[])
 	printf("\n%ld\n", sizeof(tree) / sizeof(int));
 
 
+#if 0
 	// 更新data[]中下标=4(idx=4)的值为40
 	update(0, 0, sizeof(data) / sizeof(int) - 1, 4, 40);
 	for ( int i = 0; i < sizeof(tree) / sizeof(int); i++ )
 	{
 		printf("%d ", tree[i]);
 	}
+#endif
 
-	printf("\n%ld\n", sizeof(tree) / sizeof(int));
+
+	
+	int res = query(0, 0, sizeof(data) / sizeof(int) - 1, 4, 6);
+	printf("query = %d\n", res);
 
 	return 0;
 
