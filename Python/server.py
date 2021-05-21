6a3ee6466944cff93f71637183f4dcfae1c73a18
infuq <infuq217@outlook.com>
2021-05-21 16:14:57 +0800

    ...

---
+++ Python/server.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+res = {'result': 'this is a test'}
+host = ('127.0.0.1', 8080)
+
+
+class Resquest(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        # 解析请求参数
+        if '?' in self.path:
+            data = self.path.split('?', 1)[1] + '\n'
+            print(data)
+            with open('./data.txt', 'ab+') as f:
+                f.writelines([data.encode()])
+
+        # 响应
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(res).encode())
+
+
+if __name__ == '__main__':
+    server = HTTPServer(host, Resquest)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
