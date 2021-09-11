7b49ec833a70e1c808081c36199de42d505e992d
infuq <infuq217@outlook.com>
2021-09-11 23:43:45 +0800

    ...

--- Java/infuq-v/src/main/java/com/infuq/tmp/Main.java
+++ Java/infuq-v/src/main/java/com/infuq/tmp/Main.java
@@ -2,23 +2,19 @@ package com.infuq.tmp;
 
 import java.util.logging.Logger;
 
-public class Main{
-
+public class Main {
+    
     public static void main(String args[]) {
-        
-        
+                
         Logger.getLogger("com.infuq.tmp.Main").info("Java");
+
 
         for (;;) {
             try {
-                Thread.sleep(1000_15);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
-
-
 }
