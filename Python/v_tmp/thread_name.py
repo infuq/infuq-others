1d67b24f9b2c89fe31d701407dce05c758752c6d
infuq <infuq217@outlook.com>
2021-10-04 01:30:20 +0800

    '测试python线程'

---
+++ Python/v_tmp/thread_name.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+
+import threading
+
+class MyThread(threading.Thread):
+
+    def __init__(self,name):
+        super(MyThread,self).__init__(name=name) #python2情况
+
+    def run(self):
+        print "{} is running... tid={}".format(self.name, 3)
+
+if __name__ == '__main__':
+    thread = MyThread("v-thread_name")
+    thread.start()
