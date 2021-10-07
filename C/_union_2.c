eb49025aaed0b4e159b3d183b8d152bf0cc03ca9
infuq <infuq217@outlook.com>
2021-10-07 19:58:50 +0800

    '...'

---
+++ C/_union_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+union Un
+{
+    char i;
+    int year;
+};
+
+int main(int argc, char *argv)
+{
+
+    union Un un;
+
+    printf("%ld\n", sizeof(un));
+
+    printf("%p\n", &un);
+    printf("%p\n", &(un.i));
+    printf("%p\n", &(un.year));
+
+    return 0;
+}
