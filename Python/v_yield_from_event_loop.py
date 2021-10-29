49cacca33a075cb0e7e401f9e4c59b9175cbf666
infuq <infuq217@outlook.com>
2021-10-29 22:06:46 +0800

    '...'

--- Python/v_yield_from_event_loop.py
+++ Python/v_yield_from_event_loop.py
@@ -3,9 +3,15 @@
 import socket
 from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
 
+'''
+原文地址
+https://www.cnblogs.com/olivertian/p/11444480.html
+'''
+
 selector = DefaultSelector()
 stopped = False
 urls_todo = {"/", "/1", "/2", "/3"}
+
 
 class Future:
     def __init__(self):
@@ -21,12 +27,9 @@ class Future:
             callback(self)
 
     def __iter__(self):
-        """
-        yield的出现使得__iter__函数变成一个生成器，生成器本身就有next方法，所以不需要额外实现。
-        yield from x语句首先调用iter(x)获取一个迭代器（生成器也是迭代器）
-        """
-        yield self  # 外面使用yield from把f实例本身返回
-        return self.result  # 在Task.step中send(result)的时候再次调用这个生成器，但是此时会抛出stopInteration异常，并且把self.result返回
+        yield self
+        return self.result
+
 
 def connect(sock, address):
     f = Future()
@@ -43,6 +46,7 @@ def connect(sock, address):
     yield from f
     selector.unregister(sock.fileno())
 
+
 def read(sock):
     f = Future()
 
@@ -50,13 +54,10 @@ def read(sock):
         f.set_result(sock.recv(4096))
 
     selector.register(sock.fileno(), EVENT_READ, on_readable)
-    """
-    此处的chunck接收的是f中return的f.result，同时会跑出一个stopIteration的异常，只不过被yield from处理了。
-    这里也可直接写成chunck = yiled f
-    """
-    chunck = yield from f
+    chunk = yield from f
     selector.unregister(sock.fileno())
-    return chunck
+    return chunk
+
 
 def read_all(sock):
     response = []
@@ -65,6 +66,7 @@ def read_all(sock):
         response.append(chunk)
         chunk = yield from read(sock)
     return b"".join(response)
+
 
 class Crawler:
     def __init__(self, url):
@@ -83,21 +85,21 @@ class Crawler:
         if not urls_todo:
             stopped = True
 
+
 class Task:
     def __init__(self, coro):
-        self.coro = coro
+        self._coro = coro
         f = Future()
         f.set_result(None)
-        self.step(f)  # 激活Task包裹的生成器
+        self.step(f)
 
     def step(self, future):
         try:
-            # next_future = self.coro.send(future.result)
-            next_future = self.coro.send(None)  # 驱动future
+            next_future = self._coro.send(None)
         except StopIteration:
             return
-
         next_future.add_done_callback(self.step)
+
 
 def loop():
     while not stopped:
@@ -106,11 +108,13 @@ def loop():
             callback = event_key.data
             callback()
 
+
 if __name__ == "__main__":
     import time
     start = time.time()
     for url in urls_todo:
         crawler = Crawler(url)
-        Task(crawler.fetch())
+        coro = crawler.fetch()
+        Task(coro)
     loop()
     print(time.time() - start)
