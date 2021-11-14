6269c9773a35edf87b1b3ff6040b8104863b5926
infuq <infuq217@outlook.com>
2021-11-14 14:03:22 +0800

    '...'

--- C/2021-11-14/child_thread_exit.c
+++ C/2021-11-14/child_thread_exit.c
@@ -10,7 +10,6 @@ void *thread(void *arg)
     newthid = pthread_self();
     printf("this is a new thread, thread ID = %ld\n", newthid);
     
-    sleep(10);
 
 //    pthread_exit(NULL);
 
@@ -28,9 +27,8 @@ int main(void)
         exit(1);
     }
    
-    pthread_detach(thid);
 
     sleep(30);
-    pthread_exit(NULL);
+
     return 0;
 }
