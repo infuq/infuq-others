4c687840a206eb956bddf167222765a1b6c025c0
infuq <infuq217@outlook.com>
2022-01-08 03:22:26 +0800

    ...

--- Python/v-tmp.py
+++ Python/v-tmp.py
@@ -1,32 +1,13 @@
 #! /usr/bin/env python
 # -*- coding:utf8 -*-
 
+import shutil
 
-from socket import *
 
 if __name__ == '__main__':
 
-    
-    import shutil
-    import os
-    
-    # shutil.rmtree("d:/!CloudShell")
+    shutil.rmtree("D:/t")
 
-    ip=gethostbyname(gethostname())
-    print(ip)
-    
-    # server = socket(AF_INET, SOCK_STREAM)
-    # server.bind((ip, 8081))
-    # server.listen(10)
-    
-
-    while False:
-        try:
-            cli,addr = server.accept()
-            # res = cli.recv(1024)
-            # cli.send(res)
-        except Exception as x:
-            pass
 
 
 
--- Python/v_tmp/host_ip.py
+++ Python/v_tmp/host_ip.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
 import socket
+from socket import *
 
 """
 查询本机ip地址    
 """
 
+# 方式一
 def host_ip():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,11 +19,20 @@ def host_ip():
 
 
 def host_ip_2():
-    import socket
-    hostname=socket.gethostname()
-    print(hostname)
-    ip=socket.gethostbyname(hostname)
-    return ip
+    # 方式二
+    if 1 < 0:
+        import socket
+        hostname=socket.gethostname()
+        print(hostname)
+        ip=socket.gethostbyname(hostname)
+        return ip
+    
+    # 方式三
+    if -1 < 0:
+        # from socket import *
+        ip=gethostbyname(gethostname())
+        return ip
+
 
 if __name__ == '__main__':
     print(host_ip_2())
