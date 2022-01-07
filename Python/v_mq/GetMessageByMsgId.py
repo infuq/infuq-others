9ab089233440da96329dc50cd9e31c973f08ba74
infuq <infuq217@outlook.com>
2022-01-07 20:00:35 +0800

    ...

---
+++ Python/v_mq/GetMessageByMsgId.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding=utf8
+
+'''
+pip install aliyun-python-sdk-core
+pip install aliyun-python-sdk-ecs
+pip install aliyun-python-sdk-ons
+'''
+
+import json
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkons.request.v20190214.OnsMessageGetByMsgIdRequest import OnsMessageGetByMsgIdRequest
+
+
+# 文档 https://help.aliyun.com/document_detail/141780.html
+# https://api.aliyun.com/?spm=5176.smartservice_service_robot_chat_new.0.0.37843cdaaWBcgI#/?product=Ons&version=2019-02-14&api=OnsMessageGetByMsgId&params={}&tab=DEMO&lang=PYTHON
+
+
+def get_message_by_msg_id():
+
+
+    access_id=""
+    access_key=""
+    instance_id = ""
+    region = "cn-shanghai"
+
+    topic_name = ""
+    message_id = ""
+    
+
+    client = AcsClient(access_id, access_key, region)
+    request = OnsMessageGetByMsgIdRequest()
+    request.set_accept_format('json')
+    request.set_Topic(topic_name)
+    request.set_InstanceId(instance_id)
+    request.set_MsgId(message_id)
+
+    response = client.do_action_with_exception(request)
+    json_data = str(response, encoding='utf-8')
+    message_obj = json.loads(json_data)
+    print(message_obj['Data']['Body'])
+
+
+
+if __name__ == '__main__':
+    get_message_by_msg_id()
+    
--- Python/v_mq/producer.py
+++ Python/v_mq/producer.py
@@ -14,36 +14,33 @@ from mq_http_sdk.mq_client import *
 
 # 文档 https://help.aliyun.com/document_detail/141780.html
 
+
+
 def send():
 
-    # 初始化client
-    mq_client = MQClient(
-        host="", # 例如公网HTTP http://1841829814603902.mqrest.cn-yinchuan.aliyuncs.com
-        access_id="",
-        access_key=""
-        )
+    host = ""  # 例如公网HTTP http://1841829814603902.mqrest.cn-yinchuan.aliyuncs.com
+    access_id = ""
+    access_key = ""
+    instance_id = ""
 
-    topic_name = "<TOPIC名称>"
+    topic_name = "" # TOPIC名称
     instance_id = "" # 实例ID
+
+    message_body = '{"year":1993,"address":"yinchuan"}' # 消息内容
+    message_tag = "<TAG>" # 消息标签
+    message_key = ""
+
+    # 初始化client
+    mq_client = MQClient(host=host, access_id=access_id, access_key=access_key)
 
     producer = mq_client.get_producer(instance_id, topic_name)
     
-    # 循环发布多条消息
     msg_count = 1
-
     try:
         for i in range(msg_count):
-        
-            msg = TopicMessage(
-                # 消息内容
-                '{"year":1993,"address":"yinchuan"}', 
-                # 消息标签
-                "<TAG>"
-                )
-            # 设置属性
-            msg.put_property("self_key", "self_value")
-            # 设置KEY
-            msg.set_message_key("DH82691028")
+            msg = TopicMessage(message_body, message_tag)           
+            msg.put_property("self_key", "self_value")  # 设置属性      
+            msg.set_message_key(message_key)
             re_msg = producer.publish_message(msg)
             print("Publish Message Succeed. MessageID:%s, BodyMD5:%s" % (re_msg.message_id, re_msg.message_body_md5))
 
