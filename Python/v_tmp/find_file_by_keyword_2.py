6f8fcfcb8ac5f5fea9db0f6900c4bd707663ce85
infuq <infuq217@outlook.com>
2022-01-08 00:43:52 +0800

    ...

--- Python/v_tmp/find_file_by_keyword_2.py
+++ Python/v_tmp/find_file_by_keyword_2.py
@@ -8,16 +8,24 @@ import re
 import os
 import sys
 
-# 返回指定目录下的文件
 def dir(root, suffix):
     files = os.listdir(root)
     for file in files:
         if not os.path.isfile(os.path.join(root, file)):
             continue
-        # if suffix and os.path.splitext(file)[-1][1:] != suffix[1:]: # 如果指定了文件后缀则校验, 否则不校验
-        if suffix and file.endswith(suffix):
+        if suffix and os.path.splitext(file)[-1][1:] != suffix[1:]: # 如果指定了文件后缀则校验, 否则不校验
             continue
         yield os.path.join(root, file)
+
+# 返回指定目录下的文件 & 递归
+def dir_recursion(root, suffix):
+    for root, dirs, files in os.walk(root, topdown=True):
+        for file in files:
+            if file.endswith('.txt'):
+                print('这是一个txt文件')
+            print(os.path.join(root, file))
+        for dir in dirs:
+            print(os.path.join(root, dir))          
 
 
 def find_file_by_keyword(directory, keyword, suffix=None):
@@ -41,3 +49,4 @@ if __name__ == '__main__':
     # 查找指定目录下包含指定关键字的文件
     find_file_by_keyword(directory, keyword)
 
+  
