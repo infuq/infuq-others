6b2460bab5963c344570d395c9d7c2ae6b0e7a21
infuq <infuq217@outlook.com>
2022-01-08 02:28:20 +0800

    zookeeper

--- Python/v_zookeeper/zk.py
+++ Python/v_zookeeper/zk.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
  
+"""
+pip install kazoo
+"""
+
 import sys
 from kazoo.client import KazooClient
 from urllib import parse
@@ -14,20 +18,38 @@ def main():
         zkc = KazooClient(hosts=host + ':' + port, timeout=timeout)
         zkc.start()
 
+        print('[ zk状态 ]->{}'.format(zkc.state))
+
+        # node = zkc.get_children('/')
         # node = zkc.get_children('/dubbo')
         # print(node)
 
-        # node = zkc.get("/dubbo/com.infuq.facade.QueryUserInfoFacade")
+        service_name = 'com.infuq.facade.QueryUserInfoFacade'
+
+        # node = zkc.get("/dubbo/{}/".format(service_name))
+        # node = zkc.get_children("/dubbo/{}".format(service_name))
         # print(node)
 
-        # node = zkc.get_children("/dubbo/com.infuq.facade.QueryUserInfoFacade/")
-        # print(node)
+        
+        # 提供者信息
+        providers = zkc.get_children('/dubbo/{}/providers'.format(service_name))
+        for _provider in providers:
+            provider = parse.unquote(_provider)
+            print('[ 提供者 ]->{}'.format(provider))
+            # 获取提供者IP和端口
+            ip = provider.split("/")[2].split(':')[0]
+            print('[ 提供者IP ]->{}'.format(ip))
+            port = provider.split("/")[2].split(':')[1]
+            print('[ 提供者端口 ]->{}'.format(port))
 
-        provider = 'com.infuq.facade.QueryUserInfoFacade'
-        node = zkc.get_children('/dubbo/{}/providers'.format(provider))
-        print(node)
-        for v in node:
-            print(parse.unquote(v))
+        # 消费者信息
+        consumers = zkc.get_children('/dubbo/{}/consumers'.format(service_name))
+        for _consumer in consumers:
+            consumer = parse.unquote(_consumer)
+            print('[ 消费者 ]->{}'.format(consumer))
+            # 获取消费者IP和端口
+            ip = consumer.split("/")[2].split(':')[0]
+            print('[ 消费者IP ]->{}'.format(ip))
 
 
 
