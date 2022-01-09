3fcc67a831212b431d973a24186587fa46282b70
infuq <infuq217@outlook.com>
2022-01-09 15:17:23 +0800

    mmap

---
+++ C/2022-1-9/v_mmap.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+int main(int argc, const char *argv[])
+{
+
+    int fd = open("t.txt", O_RDWR);
+    int len = lseek(fd, 0, SEEK_END);
+
+    void* ptr = mmap(NULL, len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    if (ptr == MAP_FAILED)
+    {
+        perror("mmap error");
+        exit(1);
+    }
+    // 打印内容
+    printf("%s", (char *)ptr);
+
+
+    munmap(ptr, len);
+    close(fd);
+
+	return 0;
+
+}
