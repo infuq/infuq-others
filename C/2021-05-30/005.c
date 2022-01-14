7ae441f10d2bfe27fb1ab7e69713c729efae7746
infuq <infuq217@outlook.com>
2022-01-15 03:10:21 +0800

    ...

--- C/2021-05-30/005.c
+++ C/2021-05-30/005.c
@@ -7,12 +7,21 @@ int main()
     char arr1[] = "abcdef";
     char arr2[] = "abcdef";
 
+    printf("&arr1=%p\n", &arr1);
+    printf("&arr2=%p\n", &arr2);
+
+    char *ptr1 = arr1;
+    *ptr1 = 'K';
+    printf("arr1=%s\n\n", arr1);
+
+
+
     // p1和p2指向的内容存储在内存的静态存储区
     char *p1 = "abcdef";
     char *p2 = "abcdef";
-    
-    printf("&arr1=%p\n", &arr1);
-    printf("&arr2=%p\n\n", &arr2);
+   
+    // 由于"abcdef"是常量字符串, 不可以被修改, 因此这里的赋值会报错
+    *p1 = 'Q';
 
     printf("p1=%p\n", p1);
     printf("p2=%p\n", p2);
