7ded0a84b8dd5b2a470aae8f6ef8e0f127e46d80
infuq <infuq217@outlook.com>
2022-02-24 20:19:03 +0800

    ...

--- C/2022-2-24/arr_sizeof.c
+++ C/2022-2-24/arr_sizeof.c
@@ -4,19 +4,20 @@
 
 void count(int arr[])
 {
-    printf("size=%ld\n", sizeof(arr)); // 8
+    // 不论arr的容量是多少,sizeof(arr)始终等于sizeof(int *)
+    printf("size=%ld\n", sizeof(arr)); // 8字节
 }
 
 int main(int argc, char **argv)
 {
 
     int arr[] = {1, 2, 5, 7};
-    printf("size=%ld\n", sizeof(arr)); // 16
+    printf("size=%ld\n", sizeof(arr)); // 16字节
 
     count(arr);
 
     int *ptr = NULL;
-    printf("size=%ld\n", sizeof(ptr)); // 8
+    printf("size=%ld\n", sizeof(ptr)); // 8字节
 
 }
 
