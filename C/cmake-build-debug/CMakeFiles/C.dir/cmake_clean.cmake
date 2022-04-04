c4c5482f6d7ed1c209b553322f34804fb36cb266
infuq <infuq217@outlook.com>
2022-04-04 21:16:31 +0800

    ...

---
+++ C/cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C.exe"
+  "C.exe.manifest"
+  "C.pdb"
+  "CMakeFiles/C.dir/network/_reactor.c.obj"
+  "CMakeFiles/C.dir/network/_reactor.c.obj.d"
+  "libC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
