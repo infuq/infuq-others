5992f77157a216dc37313d609364a077850616cb
infuq <infuq217@outlook.com>
2022-05-24 01:21:40 +0800

    ...

---
+++ OS/10-UserProcess6/process/process.h
@@ -0,0 +1,18 @@
+#ifndef __USER_PROCESS_H 
+#define __USER_PROCESS_H
+
+#include "thread.h"
+#include "stdint.h"
+
+#define default_prio 31
+#define USER_STACK3_VADDR  (0xc0000000 - 0x1000)
+#define USER_VADDR_START 0x8048000
+
+void process_execute(void *func, char *name);
+void start_process(void *func);
+void process_activate(struct task_struct *p_thread);
+void page_dir_activate(struct task_struct *p_thread);
+uint32_t *create_page_dir();
+void create_user_vaddr_bitmap(struct task_struct *user_process);
+
+#endif
