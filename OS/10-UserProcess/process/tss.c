ff931bd3ccb8d82cf880c578b0d353c1fb200ebf
infuq <infuq217@outlook.com>
2022-05-28 12:21:22 +0800

    ...

--- OS/10-UserProcess/process/tss.c
+++ OS/10-UserProcess/process/tss.c
@@ -52,13 +52,15 @@ void update_tss_esp(struct task_struct *pthread)
 static struct gdt_desc make_gdt_desc(uint32_t *desc_addr, uint32_t limit, uint8_t attr_low, uint8_t attr_high)
 {
     uint32_t    desc_base   = (uint32_t) desc_addr;
+
     struct gdt_desc desc;
-    desc.limit_low_word         = limit & 0x0000ffff;
-    desc.base_low_word          = desc_base & 0x0000ffff;
-    desc.base_mid_byte          = ( (desc_base & 0x00ff0000) >> 16);
-    desc.attr_low_byte          = (uint8_t) (attr_low);
-    desc.limit_high_attr_high   = ( ( (limit & 0x000f0000) >> 16) + (uint8_t) (attr_high) );
-    desc.base_high_byte         = desc_base >> 24;
+    desc.limit_low_word = limit & 0x0000ffff;
+    desc.base_low_word = desc_base & 0x0000ffff;
+    desc.base_mid_byte = ( (desc_base & 0x00ff0000) >> 16);
+    desc.attr_low_byte = (uint8_t) (attr_low);
+    desc.limit_high_attr_high = ( ( (limit & 0x000f0000) >> 16) + (uint8_t) (attr_high) );
+    desc.base_high_byte = desc_base >> 24;
+    
     return desc;
 }
 
