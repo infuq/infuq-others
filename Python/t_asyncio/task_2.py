6e77a941ad97ffc3361c432473dc36a1ae4f8309
infuq <infuq217@outlook.com>
2024-07-09 18:29:51 +0800

    '...'

--- Python/t_asyncio/task_2.py
+++ Python/t_asyncio/task_2.py
@@ -13,19 +13,16 @@ async def mock():
     # 执行完成之后, 会向 _ready 中添加一个任务(Task.task_wakeup)
     return "模拟"
 
-def _task_finish_cb(ret):
-    print('3', threading.current_thread().name)
-    print('_task_finish_cb', ret)
 
 async def main():
     print('1', threading.current_thread().name)
     task = asyncio.create_task(mock()) # 向 EventLoop 中的_ready 添加一个任务
-    task.add_done_callback(_task_finish_cb)
+
     ret = await task
     print('main#', ret)
 
 
 if __name__ == '__main__':
-    # 底层会添加一个回调 _run_until_complete_cb , 回调内部会停止事件循环 self._stopping = True
+
     asyncio.run(main())
 
