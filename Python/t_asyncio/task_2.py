15a447a10fb42a9b16c974764559819c6502a085
infuq <v-infuq@outlook.com>
2024-07-11 21:47:29 +0800

    ...

--- Python/t_asyncio/task_2.py
+++ Python/t_asyncio/task_2.py
@@ -4,19 +4,31 @@ import asyncio
 import threading
 import time
 import dis
+import inspect
+from objprint import op
 
 
 async def mock():
     print('2', threading.current_thread().name)
-    await asyncio.sleep(3) # 向 EventLoop 中的 _scheduled 添加一个任务
 
-    # 执行完成之后, 会向 _ready 中添加一个任务(Task.task_wakeup)
     return "模拟"
+
+
+def _task_finish_cb(ret):
+    print('3', threading.current_thread().name)
+    print('_task_finish_cb', ret)
+    frame = inspect.currentframe()
+    op(
+        frame,
+        honor_existing=False,
+        depth=2
+    )
 
 
 async def main():
     print('1', threading.current_thread().name)
-    task = asyncio.create_task(mock()) # 向 EventLoop 中的_ready 添加一个任务
+    task = asyncio.create_task(mock())
+    task.add_done_callback(_task_finish_cb)
 
     ret = await task
     print('main#', ret)
